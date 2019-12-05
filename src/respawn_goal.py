97e618a5e48c09abab6139c0c8790154eac1b194
sagarsv04 <scoutsv.sv@gmail.com>
2019-12-05 00:07:48 -0500

    turtlebot running using dqn

---
+++ src/respawn_goal.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import rospy
+import random
+import time
+import os
+from gazebo_msgs.srv import SpawnModel, DeleteModel
+from gazebo_msgs.msg import ModelStates
+from geometry_msgs.msg import Pose
+
+class Respawn():
+    def __init__(self):
+        self.modelPath = os.path.dirname(os.path.realpath(__file__))
+        self.modelPath = self.modelPath.replace('motion_planing/src',
+                                                'motion_planing/models/turtlebot_square/goal_box/model.sdf')
+        self.f = open(self.modelPath, 'r')
+        self.model = self.f.read()
+        self.goal_position = Pose()
+        self.init_goal_x = 0.6
+        self.init_goal_y = 0.0
+        self.goal_position.position.x = self.init_goal_x
+        self.goal_position.position.y = self.init_goal_y
+        self.modelName = 'goal'
+        self.obstacle_1 = 0.6, 0.6
+        self.obstacle_2 = 0.6, -0.6
+        self.obstacle_3 = -0.6, 0.6
+        self.obstacle_4 = -0.6, -0.6
+        self.last_goal_x = self.init_goal_x
+        self.last_goal_y = self.init_goal_y
+        self.last_index = 0
+        self.sub_model = rospy.Subscriber('gazebo/model_states', ModelStates, self.checkModel)
+        self.check_model = False
+        self.index = 0
+
+    def checkModel(self, model):
+        self.check_model = False
+        for i in range(len(model.name)):
+            if model.name[i] == "goal":
+                self.check_model = True
+
+    def respawnModel(self):
+        while True:
+            if not self.check_model:
+                rospy.wait_for_service('gazebo/spawn_sdf_model')
+                spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
+                spawn_model_prox(self.modelName, self.model, 'robotos_name_space', self.goal_position, "world")
+                rospy.loginfo("Goal position : %.1f, %.1f", self.goal_position.position.x,
+                              self.goal_position.position.y)
+                break
+            else:
+                pass
+
+    def deleteModel(self):
+        while True:
+            if self.check_model:
+                rospy.wait_for_service('gazebo/delete_model')
+                del_model_prox = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
+                del_model_prox(self.modelName)
+                break
+            else:
+                pass
+
+    def getPosition(self, position_check=False, delete=False):
+        if delete:
+            self.deleteModel()
+        while position_check:
+            goal_x_list = [0.6, 1.9, 0.5, 0.2, -0.8, -1, -1.9, 0.5, 2, 0.5, 0, -0.1, -2]
+            goal_y_list = [0, -0.5, -1.9, 1.5, -0.9, 1, 1.1, -1.5, 1.5, 1.8, -1, 1.6, -0.8]
+
+            self.index = random.randrange(0, 13)
+            print(self.index, self.last_index)
+            if self.last_index == self.index:
+                position_check = True
+            else:
+                self.last_index = self.index
+                position_check = False
+
+            self.goal_position.position.x = goal_x_list[self.index]
+            self.goal_position.position.y = goal_y_list[self.index]
+
+        time.sleep(0.5)
+        self.respawnModel()
+
+        self.last_goal_x = self.goal_position.position.x
+        self.last_goal_y = self.goal_position.position.y
+
+        return self.goal_position.position.x, self.goal_position.position.y
